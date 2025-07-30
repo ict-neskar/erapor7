f28f3adebb19ac97eb9c4d383955bd20f7e6e3d6
satoufuyuki <satoufuyuki@proton.me>
2025-07-30 11:49:01 +0700

    chore: remove this

--- entrypoint.d/98-init-storage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -ex
-
-mkdir -p /var/www/html/storage/app/public && \
-    mkdir -p /var/www/html/storage/framework/cache && \
-    mkdir -p /var/www/html/storage/framework/sessions && \
-    mkdir -p /var/www/html/storage/framework/testing && \
-    mkdir -p /var/www/html/storage/framework/views && \
-    mkdir -p /var/www/html/storage/logs
